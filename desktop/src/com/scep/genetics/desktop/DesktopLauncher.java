395ff5f83b460d0c1aa6a7258bc2ab8b5390eca9
Bjornir90 <celestincollin@gmail.com>
2019-10-19 14:28:16 +0100

    Set default window size to 1920x1080

--- desktop/src/com/scep/genetics/desktop/DesktopLauncher.java
+++ desktop/src/com/scep/genetics/desktop/DesktopLauncher.java
@@ -10,6 +10,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		DesktopMini2DxConfig config = new DesktopMini2DxConfig(WarriorsGame.GAME_IDENTIFIER);
 		config.vSyncEnabled = true;
+		config.title = "Genetics Warriors";
+		config.width = 1920;
+		config.height = 1080;
 		new DesktopMini2DxGame(new WarriorsGame(), config);
 	}
 }
