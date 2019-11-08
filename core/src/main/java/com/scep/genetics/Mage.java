273e07c56452acee229819235919740a5093e243
Elian <elianraynaud@gmail.com>
2019-11-08 10:34:59 +0100

    Defining cst rates

--- core/src/main/java/com/scep/genetics/Mage.java
+++ core/src/main/java/com/scep/genetics/Mage.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Mage extends Fighter {
 
+    final static float DAMAGE_RATE = 0.4f;
+    final static float HEALTH_RATE = 2;
+    final static float ATQ_SPEED_RATE = 0.008f;
+    final static float MOV_SPEED_RATE = 0.05f;
+    final static float CRIT_CHANC_RATE = 0.55f;
+    final static float CRIT_DMG_RAGE = 3;
 
     public Mage(String spritePath) {
 
@@ -21,12 +27,12 @@ public class Mage extends Fighter {
 
     @Override
     public void setCarac(List<Integer> pts) throws IndexOutOfBoundsException{
-        this.damage += (int) (0.4*pts.get(0));
-        this.initial_health += (int) (2*pts.get(1));
-        this.secondPerAttack -= 0.008*pts.get(2);
-        this.mov_speed = this.mov_speed * 0.05 * pts.get(3);
-        this.crit_chance += 0.55*pts.get(4);
-
+        this.damage += (int) (DAMAGE_RATE * pts.get(0));
+        this.initial_health += (int) (HEALTH_RATE * pts.get(1));
+        this.secondPerAttack -= ATQ_SPEED_RATE * pts.get(2);
+        this.mov_speed = this.mov_speed * MOV_SPEED_RATE * pts.get(3);
+        this.crit_chance += CRIT_CHANC_RATE * pts.get(4);
+        this.crit_damage += CRIT_DMG_RAGE * pts.get(5);
     }
 
     @Override
